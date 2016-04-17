b32d3887a45ff1c00838e8ea46b4f523a016fc0c
sondabar <lars@cormann.biz>
2016-04-18 00:21:11 +0200

    Preconfigure kubernetes cloud

--- docker/jenkins-master/config.groovy
+++ docker/jenkins-master/config.groovy
@@ -1,3 +1,14 @@
-import jenkins.model.*
+import jenkins.model.Jenkins
+import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud
+import org.csanchez.jenkins.plugins.kubernetes.PodTemplate
 
+// disable executors on master
 Jenkins.instance.setNumExecutors(0)
+
+// Configure kubernetes plugin
+PodTemplate podTemplate = new PodTemplate("sondabar/jenkins-slave-jnlp", Collections.emptyList())
+podTemplate.setName("jenkins-slave-jnlp")
+podTemplate.setInstanceCap(10)
+podTemplate.setLabel("")
+KubernetesCloud kubernetesCloud = new KubernetesCloud("ci-cloud", Collections.singletonList(podTemplate), "https://kubernetes.default", "ci", "http://jenkins-master.ci", "10", 10, 5, 5);
+Jenkins.instance.clouds.add(kubernetesCloud)
