2c723387037df5c8115f6d84759c159f2af7c409
sondabar <lars@cormann.biz>
2016-04-20 00:45:18 +0200

    Add jenkins default user

--- docker/jenkins-master/jenkins.sh
+++ docker/jenkins-master/jenkins.sh
@@ -9,13 +9,13 @@ set -e
 copy_reference_file() {
 	f="${1%/}"
 	b="${f%.override}"
-	echo "$f" >> "$COPY_REFERENCE_FILE_LOG"
+	echo "$f" >> "${COPY_REFERENCE_FILE_LOG}"
 	rel="${b:23}"
 	dir=$(dirname "${b}")
-	echo " $f -> $rel" >> "$COPY_REFERENCE_FILE_LOG"
+	echo " $f -> $rel" >> "${COPY_REFERENCE_FILE_LOG}"
 	if [[ ! -e /var/jenkins_home/${rel} || $f = *.override ]]
 	then
-		echo "copy $rel to JENKINS_HOME" >> "$COPY_REFERENCE_FILE_LOG"
+		echo "copy $rel to JENKINS_HOME" >> "${COPY_REFERENCE_FILE_LOG}"
 		mkdir -p "/var/jenkins_home/${dir:23}"
 		cp -r "${f}" "/var/jenkins_home/${rel}";
 		# pin plugins on initial copy
@@ -24,12 +24,12 @@ copy_reference_file() {
 }
 export -f copy_reference_file
 touch "${COPY_REFERENCE_FILE_LOG}" || (echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?" && exit 1)
-echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
+echo "--- Copying files at $(date)" >> "${COPY_REFERENCE_FILE_LOG}"
 find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  exec /bin/bash -c "java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS $@"
+  exec /bin/bash -c "java ${JAVA_OPTS} -jar /usr/share/jenkins/jenkins.war ${JENKINS_OPTS} $@"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
