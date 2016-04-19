da30543f3d164136045363228cffb55ec7064b8b
sondabar <lars@cormann.biz>
2016-04-20 00:50:12 +0200

    enable security

--- docker/jenkins-master/config.groovy
+++ docker/jenkins-master/config.groovy
@@ -1,9 +1,14 @@
+import hudson.security.HudsonPrivateSecurityRealm
 import jenkins.model.Jenkins
 import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud
 import org.csanchez.jenkins.plugins.kubernetes.PodTemplate
 
 // disable executors on master
 Jenkins.instance.setNumExecutors(0)
+
+// enable security
+Jenkins.instance.setSecurityRealm(new HudsonPrivateSecurityRealm(false));
+Jenkins.instance.setAuthorizationStrategy(new HudsonPrivateSecurityRealm(false))
 
 // Configure kubernetes plugin
 PodTemplate podTemplate = new PodTemplate("sondabar/jenkins-slave-jnlp", Collections.emptyList())
