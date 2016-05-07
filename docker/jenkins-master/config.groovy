9fec6054027eec048ea61af663ed30c262c07c9a
sondabar <lars@cormann.biz>
2016-05-07 11:53:19 +0200

    Enable jenkins security

--- docker/jenkins-master/config.groovy
+++ docker/jenkins-master/config.groovy
@@ -1,14 +1,25 @@
+import hudson.security.GlobalMatrixAuthorizationStrategy
 import hudson.security.HudsonPrivateSecurityRealm
 import jenkins.model.Jenkins
 import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud
 import org.csanchez.jenkins.plugins.kubernetes.PodTemplate
 
+def env = System.getenv()
+def password = env['JENKINS_PASSWORD']
+
+def instance = Jenkins.getInstance()
+
 // disable executors on master
-Jenkins.instance.setNumExecutors(0)
+instance.setNumExecutors(0)
 
 // enable security
-Jenkins.instance.setSecurityRealm(new HudsonPrivateSecurityRealm(false));
-Jenkins.instance.setAuthorizationStrategy(new HudsonPrivateSecurityRealm(false))
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin",password)
+instance.setSecurityRealm(hudsonRealm)
+def strategy = new GlobalMatrixAuthorizationStrategy()
+strategy.add(Jenkins.ADMINISTER, "admin")
+instance.setAuthorizationStrategy(strategy)
+instance.save()
 
 // Configure kubernetes plugin
 PodTemplate podTemplate = new PodTemplate("sondabar/jenkins-slave-jnlp", Collections.emptyList())
@@ -17,3 +28,5 @@ podTemplate.setInstanceCap(10)
 podTemplate.setNodeSelector("")
 KubernetesCloud kubernetesCloud = new KubernetesCloud("ci-cloud", Collections.singletonList(podTemplate), "https://kubernetes.default", "ci", "http://jenkins-master.ci", "10", 10, 5, 5);
 Jenkins.instance.clouds.add(kubernetesCloud)
+
+Jenkins.instance.save()
